3f9d0dc8c3f206feea95b0cf70f0e19c37e0e349
fionaschmidiger <fiona_schmidiger@hotmail.com>
2021-07-18 10:50:38 +0200

    New class: Address

---
+++ Entity/Address.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contactmanager
+{
+    public class Address
+    {
+        public string Street { get; set; }
+        public int HouseNumber { get; set; }
+        public int PLZ { get; set; }
+        public string Village { get; set; } // Ortschaft
+        public string Country { get; set; }
+
+        public Address(string street, int houseNumber, int plz, string village,
+            string country)
+        {
+            Street = street;
+            HouseNumber = houseNumber;
+            PLZ = plz;
+            Village = village;
+            Country = country;
+        }
+
+    }
+}

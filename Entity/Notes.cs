2e37ac6f5132da1601ee06583fde92e6e4c4bf0f
fionaschmidiger <fiona_schmidiger@hotmail.com>
2021-07-18 10:58:26 +0200

    New class: Notes

---
+++ Entity/Notes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contactmanager
+{
+    public class Notes
+    {
+        public DateTime TimeStamp { get; set; }
+        public string Comment { get; set; }
+
+        public Notes(DateTime timeStamp)
+        {
+            TimeStamp = timeStamp;
+            Comment = "";
+        }
+
+    }
+}

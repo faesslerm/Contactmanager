30b18c34d7818500fe75449bc48e4dcf087ae4d3
fionaschmidiger <fiona_schmidiger@hotmail.com>
2021-08-16 21:06:05 +0200

    Class History erstellt

---
+++ Entity/History.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contactmanager
+{
+    class History
+    {
+        //Properties:
+
+        public DateTime Editing { get; set; }
+        public string Target { get; set; } //Was wurde geändert? vgl. Note unten
+
+        //Konstruktor:
+        public History(DateTime editing, string target)
+        {
+            Editing = editing;
+            Target = target;
+        }
+
+        //Methode zur Überprüng, was geändert wurde --> mit SwitchCase bspw. Adressänderung oderso?
+    }
+}
